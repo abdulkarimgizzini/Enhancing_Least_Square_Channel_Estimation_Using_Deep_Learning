38639483b9a28e6d669944d226efc51f10ace7e2
Abdul Karim Gizzini <78584545+PhD-AKG92@users.noreply.github.com>
2021-05-04 13:07:03 +0200

    Update Process_Training_Data.m

--- MatLab_Codes/Process_Training_Data.m
+++ MatLab_Codes/Process_Training_Data.m
@@ -2,19 +2,21 @@ clc;
 clearvars;
 close all;
 
-n_snr =7;
-
-load(['Data\DNN_Dataset\HLS_',num2str(n_snr),'.mat'], 'Hf', 'Hfe_LS');
-
+SNR_p = 0:5:30;
 k = 52;
 X = zeros(k*2, size(Hf,2));
 Y = zeros(k*2, size(Hf,2));
-for i = 1:size(X,2)
-X(1:k,i)     = real(Hfe_LS(:,i));
-X(k+1:2*k,i) = imag(Hfe_LS(:,i));
-Y(1:k,i)     = real(Hf(:,i));
-Y(k+1:2*k,i) = imag(Hf(:,i));
+for n_snr = 1: size(SNR_p,2)
+
+  load(['Data\DNN_Dataset\HLS_',num2str(n_snr),'.mat'], 'Hf', 'Hfe_LS');
+
+  X(1:k,:)     = real(Hfe_LS);
+  X(k+1:2*k,:) = imag(Hfe_LS);
+  Y(1:k,:)     = real(Hf);
+  Y(k+1:2*k,:) = imag(Hf);
+
+  Preamble_Error_Correction_Dataset.('X') =  X;
+  Preamble_Error_Correction_Dataset.('Y') =  Y ;
+  save(['Data\DNN_Dataset\Dataset_' num2str(n_snr)],  'Preamble_Error_Correction_Dataset','-v7.3');
+
 end
-Preamble_Error_Correction_Dataset.('X') =  X;
-Preamble_Error_Correction_Dataset.('Y') =  Y ;
-save(['Data\DNN_Dataset\Dataset_' num2str(n_snr)],  'Preamble_Error_Correction_Dataset','-v7.3');
