4267576392c2250f87447109a2dca1705ab3a3a2
Abdul Karim Gizzini <78584545+abdulkarimgizzini@users.noreply.github.com>
2023-02-23 14:01:38 +0100

    Update Process_Training_Data.m

--- MatLab_Codes/Process_Training_Data.m
+++ MatLab_Codes/Process_Training_Data.m
@@ -1,11 +1,12 @@
 clc;
 clearvars;
 close all;
-
+N_CH  = 1000;
 SNR_p = 0:5:30;
 k = 52;
-X = zeros(k*2, size(Hf,2));
-Y = zeros(k*2, size(Hf,2));
+X = zeros(k*2, N_CH);
+Y = zeros(k*2, N_CH);
+
 for n_snr = 1: size(SNR_p,2)
 
   load(['./Dataset_',num2str(n_snr),'.mat'], 'Hf', 'Hfe_LS');
@@ -15,8 +16,8 @@ for n_snr = 1: size(SNR_p,2)
   Y(1:k,:)     = real(Hf);
   Y(k+1:2*k,:) = imag(Hf);
 
-  Preamble_Error_Correction_Dataset.('X') =  X;
-  Preamble_Error_Correction_Dataset.('Y') =  Y;
-  save(['./DNN_Dataset_' num2str(n_snr)],  'Preamble_Error_Correction_Dataset','-v7.3');
+  Preamble_Error_Correction_Dataset.('X') =  X.';
+  Preamble_Error_Correction_Dataset.('Y') =  Y.';
+  save(['./DNN_Dataset_' num2str(n_snr),'.mat'],  'Preamble_Error_Correction_Dataset');
 
 end
